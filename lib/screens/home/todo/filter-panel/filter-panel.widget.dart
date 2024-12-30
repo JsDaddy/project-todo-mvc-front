a647b630425231c22099236ea9134da3250afb61
andriikamaldinov1 <akam@jsdaddy.io>
2024-12-30 15:49:48 +0200

    feat(ref: no-ref): fix comments

--- lib/screens/home/todo/filter-panel/filter-panel.widget.dart
+++ lib/screens/home/todo/filter-panel/filter-panel.widget.dart
@@ -1,41 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../../common/button/button.widget.dart';
+import 'filter-panel-buttons/filter-panel-buttons.dart';
 import 'filter-panel.controller.dart';
-import 'filter-panel.enum.dart';
 
 class FilterPanelWidget extends GetView<FilterPanelController> {
   const FilterPanelWidget({super.key});
 
   @override
   Widget build(BuildContext context) => Obx(() {
+    final bool isMobile = MediaQuery.of(context).size.width < 600;
+
     if (controller.tasks.isEmpty) {
       return const SizedBox();
     }
 
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Text(
-            '${controller.activeCount} items left',
-            style: const TextStyle(fontSize: 14, color: Colors.grey),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                '${controller.activeCount} items left',
+                style: const TextStyle(fontSize: 14, color: Colors.grey),
+              ),
+              if (!isMobile) const FilterPanelButtons(),
+              ButtonWidget(
+                onTap: controller.clearCompleted,
+                title: 'Clear completed',
+              ),
+            ],
           ),
-          Wrap(
-            spacing: 10,
-            children: controller.filters
-                .map<Widget>((FilterType filter) => ButtonWidget(
-              title: filter.type,
-              isActive: controller.currentFilter.value == filter,
-              onTap: () => controller.setFilter(filter),
-            ))
-                .toList(),
-          ),
-          ButtonWidget(
-            onTap: controller.clearCompleted,
-            title: 'Clear completed',
-          ),
+          if (isMobile)
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FilterPanelButtons(),
+              ],
+            ),
         ],
       ),
     );
--- lib/screens/home/todo/todo.widget.dart
+++ lib/screens/home/todo/todo.widget.dart
@@ -6,17 +6,26 @@ class TodoWidget extends StatelessWidget {
   const TodoWidget({super.key});
 
   @override
-  Widget build(BuildContext context) => Container(
-      width: 600,
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey.shade300),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: const Column(
-        children: <Widget>[
-          AddTaskWidget(),
-          TaskListWidget(),
-        ],
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints(
+          maxWidth: 600,
+        ),
+        width: screenWidth - 45,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey.shade300),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: const Column(
+          children: <Widget>[
+            AddTaskWidget(),
+            TaskListWidget(),
+          ],
+        ),
       ),
     );
+  }
 }
